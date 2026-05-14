8a31f0b9dbe2b483a4b173ff4fcf2299cd540ebc
Lloyd Fernandes <lloydfernandes119@gmail.com>
2026-05-14 06:41:48 +0000

    Form

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,15 +1,10 @@
 sap.ui.define([
-  "sap/ui/core/mvc/Controller",
-  "sap/m/MessageBox"
+  "sap/ui/core/mvc/Controller"
 ],
-  function (Controller, MessageBox) {
+  function (Controller) {
     "use strict";
 
     return Controller.extend("sap.training.exc.controller.App", {
 
-      onSayHello: function () {
-        MessageBox.information("Hello World");
-      }
-
     });
-  });
+  })
